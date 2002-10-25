44b2606b6b89b4903c4ec2b742ab4ac006517ba4
adammurdoch <dev-null@apache.org>
2002-10-25 11:10:40 +0000

    VfsTask:   - Creates the FileSystemManager for the tasks to use.   - Attaches VFS logging to Ant logging.   - Cleans up the VFS when the build finishes.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@147815 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/tasks/VfsTask.java
+++ src/java/org/apache/commons/vfs/tasks/VfsTask.java
@@ -55,22 +55,25 @@
  */
 package org.apache.commons.vfs.tasks;
 
-import org.apache.tools.ant.Task;
+import org.apache.commons.logging.Log;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
-import org.apache.commons.vfs.VFS;
-import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.impl.StandardFileSystemManager;
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
 
 /**
  * Base class for the VFS Ant tasks.  Provides some utility methods.
  *
  * @author <a href="mailto:adammurdoch@apache.org">Adam Murdoch</a>
- * @version $Revision: 1.4 $ $Date: 2002/10/24 02:11:03 $
+ * @version $Revision: 1.5 $ $Date: 2002/10/25 11:10:40 $
  */
 public class VfsTask
     extends Task
 {
-    private FileSystemManager manager;
+    private static StandardFileSystemManager manager;
 
     /**
      * Resolves a URI to a file, relative to the project's base directory.
@@ -82,8 +85,142 @@ public class VfsTask
     {
         if ( manager == null )
         {
-            manager = VFS.getManager();
+            manager = new StandardFileSystemManager();
+            manager.setLogger( new AntLogger() );
+            manager.init();
+            getProject().addBuildListener( new CloseListener() );
         }
         return manager.resolveFile( getProject().getBaseDir(), uri );
+    }
+
+    /** Closes the VFS manager when the project finishes. */
+    private class CloseListener
+        implements BuildListener
+    {
+        public void buildFinished( BuildEvent event )
+        {
+            if ( manager != null )
+            {
+                manager.close();
+                manager = null;
+            }
+        }
+
+        public void buildStarted( BuildEvent event )
+        {
+        }
+
+        public void messageLogged( BuildEvent event )
+        {
+        }
+
+        public void targetFinished( BuildEvent event )
+        {
+        }
+
+        public void targetStarted( BuildEvent event )
+        {
+        }
+
+        public void taskFinished( BuildEvent event )
+        {
+        }
+
+        public void taskStarted( BuildEvent event )
+        {
+        }
+    }
+
+    /** A commons-logging wrapper for Ant logging. */
+    private class AntLogger
+        implements Log
+    {
+        public void debug( final Object o )
+        {
+            log( String.valueOf( o ), Project.MSG_DEBUG );
+        }
+
+        public void debug( Object o, Throwable throwable )
+        {
+            debug( o );
+        }
+
+        public void error( Object o )
+        {
+            log( String.valueOf( o ), Project.MSG_ERR );
+        }
+
+        public void error( Object o, Throwable throwable )
+        {
+            error( o );
+        }
+
+        public void fatal( Object o )
+        {
+            log( String.valueOf( o ), Project.MSG_ERR );
+        }
+
+        public void fatal( Object o, Throwable throwable )
+        {
+            fatal( o );
+        }
+
+        public void info( Object o )
+        {
+            log( String.valueOf( o ), Project.MSG_INFO );
+        }
+
+        public void info( Object o, Throwable throwable )
+        {
+            info( o );
+        }
+
+        public void trace( Object o )
+        {
+        }
+
+        public void trace( Object o, Throwable throwable )
+        {
+        }
+
+        public void warn( Object o )
+        {
+            log( String.valueOf( o ), Project.MSG_WARN );
+        }
+
+        public void warn( Object o, Throwable throwable )
+        {
+            warn( o );
+        }
+
+        public boolean isDebugEnabled()
+        {
+            return true;
+        }
+
+        public boolean isErrorEnabled()
+        {
+            return true;
+        }
+
+        public boolean isFatalEnabled()
+        {
+            return true;
+        }
+
+        public boolean isInfoEnabled()
+        {
+            return true;
+        }
+
+        public boolean isTraceEnabled()
+        {
+            return false;
+        }
+
+        public boolean isWarnEnabled()
+        {
+            return true;
+        }
     }
 }
