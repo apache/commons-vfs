80d369a97700f2abef9dbdaad8a1f4d58b9a8069
adammurdoch <dev-null@apache.org>
2002-11-25 05:42:08 +0000

    Use a file: URL instead of a jar: URL to test the generic provider.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@147867 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/vfs/provider/url/test/UrlProviderTestCase.java
+++ src/test/org/apache/commons/vfs/provider/url/test/UrlProviderTestCase.java
@@ -63,6 +63,7 @@ import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.impl.DefaultFileSystemManager;
 import org.apache.commons.vfs.provider.url.UrlFileProvider;
+import org.apache.commons.vfs.provider.jar.JarFileSystemProvider;
 import org.apache.commons.vfs.test.AbstractProviderTestConfig;
 import org.apache.commons.vfs.test.ProviderTestSuite;
 
@@ -70,7 +71,7 @@ import org.apache.commons.vfs.test.ProviderTestSuite;
  * Test cases for the generic provider.
  *
  * @author <a href="mailto:adammurdoch@apache.org">Adam Murdoch</a>
- * @version $Revision: 1.2 $ $Date: 2002/11/23 00:33:55 $
+ * @version $Revision: 1.3 $ $Date: 2002/11/25 05:42:08 $
  */
 public class UrlProviderTestCase
     extends AbstractProviderTestConfig
@@ -86,7 +87,8 @@ public class UrlProviderTestCase
     public void prepare( final DefaultFileSystemManager manager )
         throws Exception
     {
-        manager.addProvider( "jar", new UrlFileProvider() );
+        manager.addProvider( "file", new UrlFileProvider() );
+        manager.addProvider( "jar", new JarFileSystemProvider() );
     }
 
     /**
@@ -95,9 +97,8 @@ public class UrlProviderTestCase
     public FileObject getBaseTestFolder( final FileSystemManager manager )
         throws Exception
     {
-        final File jarFile = AbstractVfsTestCase.getTestResource( "test.jar" );
-        final String uri = "jar:" + jarFile.toURL().toExternalForm() + "!/";
-        final URL url = new URL( uri );
+        final File baseDir = AbstractVfsTestCase.getTestDirectory();
+        final URL url = baseDir.toURL();
         return manager.resolveFile( url.toExternalForm() );
     }
 }
