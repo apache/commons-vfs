bf26b71e373cb15159ae2658e86e914b5cb6116f
Robert Burrell Donkin <rdonkin@apache.org>
2003-10-13 08:45:44 +0000

    Fixed typos in apache license
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@147999 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/vfs/test/ProviderTestSuite.java
+++ src/test/org/apache/commons/vfs/test/ProviderTestSuite.java
@@ -18,11 +18,11 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
+ *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
  * 4. The names "The Jakarta Project", "Commons", and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
@@ -31,7 +31,7 @@
  *
  * 5. Products derived from this software may not be called "Apache"
  *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -75,7 +75,7 @@ import org.apache.commons.vfs.provider.local.DefaultLocalFileProvider;
  * The suite of tests for a file system.
  *
  * @author <a href="mailto:adammurdoch@apache.org">Adam Murdoch</a>
- * @version $Revision: 1.12 $ $Date: 2003/06/28 10:52:30 $
+ * @version $Revision: 1.13 $ $Date: 2003/10/13 08:42:27 $
  */
 public class ProviderTestSuite
     extends TestSetup
