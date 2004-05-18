e7ef0c0c63b5f253c307a3cb3b4d208be1664170
Mario Ivankovits <imario@apache.org>
2004-05-18 06:20:56 +0000

    PR: 29007 Submitted By: Anthony Goubard (adagoubard@chello.nl)
    Change temporary filename from
    "basename_filecount.tmp"
    to "tmp_filecount_basename".
    
    Advantage of this is to keep the "fileidentity" as the file-extension of the original file is kept.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148040 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/impl/DefaultFileReplicator.java
+++ src/java/org/apache/commons/vfs/impl/DefaultFileReplicator.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002, 2003,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -130,7 +130,9 @@ public final class DefaultFileReplicator
      */
     protected String createFilename(final String baseName)
     {
-        return baseName + "_" + getFilecount() + ".tmp";
+        // BUG29007
+        // return baseName + "_" + getFilecount() + ".tmp";
+        return "tmp_" + getFilecount() + "_" + baseName;
     }
 
     /**
@@ -157,5 +159,4 @@ public final class DefaultFileReplicator
 
         return file;
     }
-
 }
