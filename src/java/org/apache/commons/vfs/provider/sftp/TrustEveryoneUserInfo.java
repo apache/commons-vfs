25d39f56acb487207d6a783199239539f2798a23
Mario Ivankovits <imario@apache.org>
2004-05-24 20:10:08 +0000

    log ssh queries using debug/info
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148058 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/provider/sftp/TrustEveryoneUserInfo.java
+++ src/java/org/apache/commons/vfs/provider/sftp/TrustEveryoneUserInfo.java
@@ -7,9 +7,13 @@
 package org.apache.commons.vfs.provider.sftp;
 
 import com.jcraft.jsch.UserInfo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class TrustEveryoneUserInfo implements UserInfo
 {
+    private Log log = LogFactory.getLog(TrustEveryoneUserInfo.class);
+
     public String getPassphrase()
     {
         return null;
@@ -22,21 +26,26 @@ public class TrustEveryoneUserInfo implements UserInfo
 
     public boolean promptPassword(String s)
     {
+        log.info(s + " - Answer: False");
         return false;
     }
 
     public boolean promptPassphrase(String s)
     {
+        log.info(s + " - Answer: False");
         return false;
     }
 
     public boolean promptYesNo(String s)
     {
+        log.debug(s + " - Answer: Yes");
+
         // trust
         return true;
     }
 
     public void showMessage(String s)
     {
+        log.debug(s);
     }
 }
