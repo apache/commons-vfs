d4c30402508e851d335fa357d4bce8898fe89c9a
Mario Ivankovits <imario@apache.org>
2004-05-25 14:43:01 +0000

    PR: 29195
    Do not issue a "list ." on the ftp-server as this might in certain (rare) cases (e.g. uu.net) force the server to do this recursive!!
    
    in this case (".") now we issue a "list /" which means in this context the same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148062 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/provider/ftp/FtpFileObject.java
+++ src/java/org/apache/commons/vfs/provider/ftp/FtpFileObject.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002, 2003,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -58,7 +58,17 @@ final class FtpFileObject
     {
         super(name, fileSystem);
         ftpFs = fileSystem;
-        relPath = rootName.getRelativeName(name);
+        String relPath = rootName.getRelativeName(name);
+        if (".".equals(relPath))
+        {
+            // do not use the "." as path against the ftp-server
+            // e.g. the uu.net ftp-server do a recursive listing then
+            this.relPath = rootName.getPath();
+        }
+        else
+        {
+            this.relPath = relPath;
+        }
     }
 
     /**
@@ -328,7 +338,15 @@ final class FtpFileObject
      */
     protected long doGetLastModifiedTime() throws Exception
     {
-        return (fileInfo.getTimestamp().getTime().getTime());
+        Calendar timestamp = fileInfo.getTimestamp();
+        if (timestamp == null)
+        {
+            return 0L;
+        }
+        else
+        {
+            return (timestamp.getTime().getTime());
+        }
     }
 
     /**
