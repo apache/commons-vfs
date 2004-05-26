2e5b6958527e6fd08068655f60a586fa9572b63c
Mario Ivankovits <imario@apache.org>
2004-05-26 08:21:29 +0000

    NPE
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148066 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/util/Messages.java
+++ src/java/org/apache/commons/vfs/util/Messages.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002, 2003,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import java.util.ResourceBundle;
  * Formats messages.
  *
  * @author <a href="mailto:adammurdoch@apache.org">Adam Murdoch</a>
- * @version $Revision: 1.10 $ $Date: 2004/05/10 20:09:50 $
+ * @version $Revision: 1.11 $ $Date: 2004/05/26 08:21:29 $
  */
 public class Messages
 {
@@ -73,6 +73,11 @@ public class Messages
     {
         try
         {
+            if (code == null)
+            {
+                return null;
+            }
+
             final MessageFormat msg = findMessage(code);
             return msg.format(params);
         }
