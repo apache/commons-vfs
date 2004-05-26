ad457c0f1f3da8de579f8e0ee9bb246800b5212d
Mario Ivankovits <imario@apache.org>
2004-05-26 08:13:35 +0000

    create ftpClient
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148063 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/vfs/provider/ftp/FtpClientFactory.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2002, 2003,2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.vfs.provider.ftp;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.vfs.FileSystemException;
+
+import java.io.IOException;
+
+/**
+ * Create a FtpClient instance
+ *
+ * @author <a href="mailto:imario@apache.org">Mario Ivankovits</a>
+ * @version $Revision: 1.1 $ $Date: 2004/05/26 08:13:35 $
+ */
+public class FtpClientFactory
+{
+    private FtpClientFactory()
+    {
+    }
+
+    /**
+     * Creates a new connection to the server.
+     */
+    public static FTPClient createConnection(String hostname, int port, String username, String password, String workingDirectory) throws FileSystemException
+    {
+        // Determine the username and password to use
+        if (username == null)
+        {
+            username = "anonymous";
+        }
+
+        if (password == null)
+        {
+            password = "anonymous";
+        }
+
+        try
+        {
+            final FTPClient client = new FTPClient();
+
+            /* as soon as commons-1.2 will be released
+            FTPFileEntryParserFactory myFactory = FtpFileSystemConfigBuilder.getInstance().getFTPFileEntryParserFactory(getFileSystemOptions());
+            if (myFactory != null)
+            {
+            client.setParserFactory(myFactory);
+            }
+            */
+
+            try
+            {
+                client.connect(hostname, port);
+
+                int reply = client.getReplyCode();
+                if (!FTPReply.isPositiveCompletion(reply))
+                {
+                    throw new FileSystemException("vfs.provider.ftp/connect-rejected.error", hostname);
+                }
+
+                // Login
+                if (!client.login(username, password))
+                {
+                    throw new FileSystemException("vfs.provider.ftp/login.error", new Object[]{hostname, username}, null);
+                }
+
+                // Set binary mode
+                if (!client.setFileType(FTP.BINARY_FILE_TYPE))
+                {
+                    throw new FileSystemException("vfs.provider.ftp/set-binary.error", hostname);
+                }
+
+                // Change to root by default
+                // All file operations a relative to the filesystem-root
+                // String root = getRoot().getName().getPath();
+                if (workingDirectory != null)
+                {
+                    if (!client.changeWorkingDirectory(workingDirectory))
+                    {
+                        throw new FileSystemException("vfs.provider/get-attributes-no-exist.error", "/");
+                    }
+                }
+            }
+            catch (final IOException e)
+            {
+                if (client.isConnected())
+                {
+                    client.disconnect();
+                }
+                throw e;
+            }
+
+            return client;
+        }
+        catch (final Exception exc)
+        {
+            throw new FileSystemException("vfs.provider.ftp/connect.error", new Object[]{hostname}, exc);
+        }
+    }
+}
