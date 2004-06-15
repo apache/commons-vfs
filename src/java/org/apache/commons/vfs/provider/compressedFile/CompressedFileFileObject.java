9396a7f6e44f896257a8a29b8eae207c28d726f2
Mario Ivankovits <imario@apache.org>
2004-06-15 19:26:21 +0000

    base for compressed files e.g. gzip, bzip2
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148075 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/vfs/provider/compressedFile/CompressedFileFileObject.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2002, 2003,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.vfs.provider.compressedFile;
+
+import org.apache.commons.vfs.Capability;
+import org.apache.commons.vfs.FileName;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.FileType;
+import org.apache.commons.vfs.provider.AbstractFileObject;
+
+/**
+ * A compressedFile compressed file
+ *
+ * @author <a href="mailto:imario@apache.org">Mario Ivankovits</a>
+ * @version $Revision: 1.1 $ $Date: 2004/06/15 19:26:21 $
+ */
+public abstract class CompressedFileFileObject
+    extends AbstractFileObject
+    implements FileObject
+{
+    private final FileObject container;
+    private String[] children;
+
+    public CompressedFileFileObject(FileName name, FileObject container, CompressedFileFileSystem fs)
+    {
+        super(name, fs);
+        this.container = container;
+
+        // todo, add getBaseName(String) to FileName
+        String basename = container.getName().getBaseName();
+        int pos = basename.lastIndexOf('.');
+        basename = basename.substring(0, pos);
+
+        children = new String[]
+        {
+            basename
+        };
+    }
+
+    /**
+     * Returns true if this file is read-only.
+     */
+    public boolean isWriteable()
+    {
+        return getFileSystem().hasCapability(Capability.WRITE_CONTENT);
+    }
+
+    /**
+     * Returns the file's type.
+     */
+    protected FileType doGetType() throws FileSystemException
+    {
+        return this.container.getType();
+    }
+
+    /**
+     * Lists the children of the file.
+     */
+    protected String[] doListChildren()
+    {
+        return children;
+    }
+
+    /**
+     * Returns the size of the file content (in bytes).  Is only called if
+     * {@link #doGetType} returns {@link FileType#FILE}.
+     */
+    protected long doGetContentSize()
+    {
+        return -1;
+    }
+
+    /**
+     * Returns the last modified time of this file.
+     */
+    protected long doGetLastModifiedTime() throws Exception
+    {
+        return container.getContent().getLastModifiedTime();
+    }
+
+    protected FileObject getContainer()
+    {
+        return container;
+    }
+
+    public void createFile() throws FileSystemException
+    {
+        container.createFile();
+        injectType(FileType.FILE);
+    }
+}
