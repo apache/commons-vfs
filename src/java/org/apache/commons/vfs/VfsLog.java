6b2357d51e4e1d7ad99da5264307970af38f1ba0
Mario Ivankovits <imario@apache.org>
2004-06-21 11:08:23 +0000

    added missing log methods
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@148092 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/VfsLog.java
+++ src/java/org/apache/commons/vfs/VfsLog.java
@@ -21,9 +21,9 @@ import org.apache.commons.logging.Log;
  * This class is to keep the old logging behaviour (for ant-task) and to be able to
  * correctly use commons-logging.<br>
  * I hope i could remove it sometimes.
- * 
+ *
  * @author <a href="mailto:imario@apache.org">Mario Ivankovits</a>
- * @version $Revision: 1.3 $ $Date: 2004/05/21 20:54:34 $
+ * @version $Revision: 1.4 $ $Date: 2004/06/21 11:08:23 $
  */
 public class VfsLog
 {
@@ -74,6 +74,111 @@ public class VfsLog
         else if (commonslog != null)
         {
             commonslog.debug(message);
+        }
+    }
+
+    /**
+     * debug
+     */
+    public static void debug(Log vfslog, Log commonslog, String message, Throwable t)
+    {
+        if (vfslog != null)
+        {
+            vfslog.debug(message, t);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.debug(message, t);
+        }
+    }
+
+    /**
+     * info
+     */
+    public static void info(Log vfslog, Log commonslog, String message, Throwable t)
+    {
+        if (vfslog != null)
+        {
+            vfslog.info(message, t);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.info(message, t);
+        }
+    }
+
+    /**
+     * info
+     */
+    public static void info(Log vfslog, Log commonslog, String message)
+    {
+        if (vfslog != null)
+        {
+            vfslog.info(message);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.info(message);
+        }
+    }
+
+    /**
+     * error
+     */
+    public static void error(Log vfslog, Log commonslog, String message, Throwable t)
+    {
+        if (vfslog != null)
+        {
+            vfslog.error(message, t);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.error(message, t);
+        }
+    }
+
+    /**
+     * error
+     */
+    public static void error(Log vfslog, Log commonslog, String message)
+    {
+        if (vfslog != null)
+        {
+            vfslog.error(message);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.error(message);
+        }
+    }
+
+    /**
+     * fatal
+     */
+    public static void fatal(Log vfslog, Log commonslog, String message, Throwable t)
+    {
+        if (vfslog != null)
+        {
+            vfslog.fatal(message, t);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.fatal(message, t);
+        }
+    }
+
+    /**
+     * fatal
+     */
+    public static void fatal(Log vfslog, Log commonslog, String message)
+    {
+        if (vfslog != null)
+        {
+            vfslog.fatal(message);
+        }
+        else if (commonslog != null)
+        {
+            commonslog.fatal(message);
         }
     }
 }
