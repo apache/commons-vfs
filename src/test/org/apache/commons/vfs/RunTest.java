bd6efd6f97ecfcdb0532a647329c84aca5a4698c
Mario Ivankovits <imario@apache.org>
2005-05-12 20:35:11 +0000

    Documentation how to setup a VFS testserver. On the way found some problems with the webdav provider. Might be due to new apache?
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/vfs/trunk@169881 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/vfs/RunTest.java
@@ -0,0 +1,103 @@
+package org.apache.commons.vfs;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.Test;
+import junit.framework.TestResult;
+import org.apache.commons.vfs.provider.ftp.test.FtpProviderTestCase;
+import org.apache.commons.vfs.provider.http.test.HttpProviderTestCase;
+import org.apache.commons.vfs.provider.jar.test.JarProviderTestCase;
+import org.apache.commons.vfs.provider.jar.test.NestedJarTestCase;
+import org.apache.commons.vfs.provider.local.test.LocalProviderTestCase;
+import org.apache.commons.vfs.provider.res.test.ResourceProviderTestCase;
+import org.apache.commons.vfs.provider.sftp.test.SftpProviderTestCase;
+import org.apache.commons.vfs.provider.smb.test.SmbProviderTestCase;
+import org.apache.commons.vfs.provider.tar.test.NestedTarTestCase;
+import org.apache.commons.vfs.provider.tar.test.NestedTbz2TestCase;
+import org.apache.commons.vfs.provider.tar.test.NestedTgzTestCase;
+import org.apache.commons.vfs.provider.tar.test.TarProviderTestCase;
+import org.apache.commons.vfs.provider.tar.test.Tbz2ProviderTestCase;
+import org.apache.commons.vfs.provider.tar.test.TgzProviderTestCase;
+import org.apache.commons.vfs.provider.temp.test.TemporaryProviderTestCase;
+import org.apache.commons.vfs.provider.test.VirtualProviderTestCase;
+import org.apache.commons.vfs.provider.url.test.UrlProviderTestCase;
+import org.apache.commons.vfs.provider.webdav.test.WebdavProviderTestCase;
+import org.apache.commons.vfs.provider.zip.test.NestedZipTestCase;
+import org.apache.commons.vfs.provider.zip.test.ZipProviderTestCase;
+
+import java.util.Properties;
+
+public class RunTest
+{
+    public static void main(String[] args) throws Exception
+    {
+        Properties props = System.getProperties();
+        props.setProperty("test.data.src", "src/test-data");
+        props.setProperty("test.basedir", "target/test-data");
+        props.setProperty("test.policy", "src/test-data/test.policy");
+        props.setProperty("test.secure", "false");
+        props.setProperty("test.smb.uri", "smb://vfsusr:vfsusr@10.0.1.54/vfsusr/vfstest");
+        props.setProperty("test.ftp.uri", "ftp://vfsusr:vfsusr@10.0.1.54/vfstest");
+        props.setProperty("test.http.uri", "http://10.0.1.54/vfstest");
+        props.setProperty("test.webdav.uri", "webdav://vfsusr:vfsusr@10.0.1.54/vfstest");
+        props.setProperty("test.sftp.uri", "sftp://vfsusr:vfsusr@10.0.1.54/vfstest");
+
+        Test tests[] = new Test[]
+        {
+            LocalProviderTestCase.suite(),
+            VirtualProviderTestCase.suite(),
+            TemporaryProviderTestCase.suite(),
+            UrlProviderTestCase.suite(),
+            ResourceProviderTestCase.suite(),
+
+            FtpProviderTestCase.suite(),
+            HttpProviderTestCase.suite(),
+            WebdavProviderTestCase.suite(),
+            SmbProviderTestCase.suite(),
+            SftpProviderTestCase.suite(),
+
+            JarProviderTestCase.suite(),
+            NestedJarTestCase.suite(),
+            ZipProviderTestCase.suite(),
+            NestedZipTestCase.suite(),
+            TarProviderTestCase.suite(),
+            TgzProviderTestCase.suite(),
+            Tbz2ProviderTestCase.suite(),
+            NestedTarTestCase.suite(),
+            NestedTgzTestCase.suite(),
+            NestedTbz2TestCase.suite(),
+        };
+
+        TestResult result = new TestResult()
+        {
+            public void startTest(Test test)
+            {
+                System.out.println("start " + test);
+                System.out.flush();
+            }
+
+            public void endTest(Test test)
+            {
+                // System.err.println("end " + test);
+            }
+
+            public synchronized void addError(Test test, Throwable throwable)
+            {
+                throwable.printStackTrace();
+            }
+
+            public synchronized void addFailure(Test test, AssertionFailedError assertionFailedError)
+            {
+                assertionFailedError.printStackTrace();
+            }
+        };
+
+        for (int i = 0; i < tests.length; i++)
+        {
+            System.out.println("start test#" + i);
+            System.out.flush();
+
+            Test test = tests[i];
+            test.run(result);
+        }
+    }
+}
