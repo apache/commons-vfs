98269434b4453898aecab591250093bc766bf65e
Mario Ivankovits <imario@apache.org>
2005-06-06 05:43:35 +0000

    minor javadoc fix
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/vfs/trunk@180228 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/tasks/AbstractSyncTask.java
+++ src/java/org/apache/commons/vfs/tasks/AbstractSyncTask.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,6 +38,7 @@ import java.util.StringTokenizer;
  * <li>Missing source file.
  * <li>Out-of-date destination file.
  * <li>Up-to-date destination file.
+ * </ul>
  *
  * @author <a href="mailto:adammurdoch@apache.org">Adam Murdoch</a>
  * @version $Revision$ $Date$
