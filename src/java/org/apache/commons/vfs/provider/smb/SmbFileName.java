99a61cfcd1eac31a56d1c3539078467bb949ab92
Mario Ivankovits <imario@apache.org>
2005-07-12 07:08:07 +0000

    do not add domain name to uri if it is not set
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/vfs/trunk@215932 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/provider/smb/SmbFileName.java
+++ src/java/org/apache/commons/vfs/provider/smb/SmbFileName.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -67,9 +67,13 @@ public class SmbFileName
         buffer.append(share);
     }
 
+    /**
+     * put domain before username if both are set
+     */
     protected void appendCredentials(StringBuffer buffer)
     {
-        if (getUserName() != null && getUserName().length() != 0)
+        if (getDomain() != null && getDomain().length() != 0 &&
+            getUserName() != null && getUserName().length() != 0)
         {
             buffer.append(getDomain());
             buffer.append("\\");
