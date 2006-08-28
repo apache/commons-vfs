83c38156b81f93b0646f9d9a6c8c7710224bb051
Mario Ivankovits <imario@apache.org>
2006-08-28 11:17:08 +0000

    http+webdav: fixed problem with usage of username as password with UserAuthenticator
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/vfs/trunk@437662 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/vfs/provider/webdav/WebdavFileProvider.java
+++ src/java/org/apache/commons/vfs/provider/webdav/WebdavFileProvider.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -86,7 +86,7 @@ public class WebdavFileProvider
 				rootName.getHostName(),
 				rootName.getPort(),
 				UserAuthenticatorUtils.getData(authData, UserAuthenticationData.USERNAME, UserAuthenticatorUtils.toChar(rootName.getUserName())),
-				UserAuthenticatorUtils.getData(authData, UserAuthenticationData.USERNAME, UserAuthenticatorUtils.toChar(rootName.getPassword())),
+				UserAuthenticatorUtils.getData(authData, UserAuthenticationData.PASSWORD, UserAuthenticatorUtils.toChar(rootName.getPassword())),
 				fileSystemOptions);
 		}
 		finally
