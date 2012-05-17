e334971605279ed6bad36e3a54a111fd5c0184da
Gary D. Gregory <ggregory@apache.org>
2012-05-17 15:31:26 +0000

    Javadoc clean ups.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1339653 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/commons/vfs2/RandomAccessContent.java
+++ core/src/main/java/org/apache/commons/vfs2/RandomAccessContent.java
@@ -22,65 +22,71 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Random Access Content.
+ * Provides random access over content.
  */
 public interface RandomAccessContent extends DataOutput, DataInput
 {
     /**
      * Returns the current offset in this file.
-     *
-     * @return the offset from the beginning of the file, in bytes,
-     *         at which the next read or write occurs.
-     * @throws IOException if an I/O error occurs.
+     * 
+     * @return the offset from the beginning of the file, in bytes, at which the next read or write occurs.
+     * @throws IOException
+     *             if an I/O error occurs.
      */
     long getFilePointer() throws IOException;
 
     /**
-     * Sets the file-pointer offset, measured from the beginning of this
-     * file, at which the next read or write occurs.  The offset may be
-     * set beyond the end of the file. Setting the offset beyond the end
-     * of the file does not change the file length.  The file length will
-     * change only by writing after the offset has been set beyond the end
-     * of the file.
-     * <br/>
-     * <b>Notice: If you use {@link #getInputStream()} you have to reget the InputStream after
-     * calling {@link #seek(long)}</b>
-     *
-     * @param pos the offset position, measured in bytes from the
-     *            beginning of the file, at which to set the file
+     * Sets the file-pointer offset, measured from the beginning of this file, at which the next read or write occurs.
+     * <p>
+     * The offset may be set beyond the end of the file. Setting the offset beyond the end of the file does not change
+     * the file length. The file length will change only by writing after the offset has been set beyond the end of the
+     * file.
+     * </p>
+     * <p>
+     * <b>Notice: If you use {@link #getInputStream()} you have to re-get the InputStream after calling
+     * {@link #seek(long)}</b>
+     * </p>
+     * 
+     * @param pos
+     *            the offset position, measured in bytes from the beginning of the file, at which to set the file
      *            pointer.
-     * @throws IOException if <code>pos</code> is less than
-     *                     <code>0</code> or if an I/O error occurs.
+     * @throws IOException
+     *             if <code>pos</code> is less than <code>0</code> or if an I/O error occurs.
      */
     void seek(long pos) throws IOException;
 
     /**
      * Returns the length of this file.
-     *
+     * 
      * @return the length of this file, measured in bytes.
-     * @throws IOException if an I/O error occurs.
+     * @throws IOException
+     *             if an I/O error occurs.
      */
     long length() throws IOException;
 
     /**
-     * Closes this random access file stream and releases any system
-     * resources associated with the stream. A closed random access
-     * file cannot perform input or output operations and cannot be
-     * reopened.
-     * <p/>
-     * <p> If this file has an associated channel then the channel is closed
-     * as well.
-     *
-     * @throws IOException if an I/O error occurs.
+     * Closes this random access file stream and releases any system resources associated with the stream. </p>
+     * <p>
+     * A closed random access file cannot perform input or output operations and cannot be reopened.
+     * </p>
+     * <p>
+     * If this file has an associated channel then the channel is closed as well.
+     * <p>
+     * 
+     * @throws IOException
+     *             if an I/O error occurs.
      */
     void close() throws IOException;
 
     /**
      * Get the input stream.
-     * <br/>
-     * <b>Notice: If you use {@link #seek(long)} you have to reget the InputStream</b>
+     * <p>
+     * <b>Notice: If you use {@link #seek(long)} you have to re-get the InputStream</b>
+     * </p>
+     * 
      * @return the InputStream.
-     * @throws IOException if an I/O error occurs.
+     * @throws IOException
+     *             if an I/O error occurs.
      */
     InputStream getInputStream() throws IOException;
 }
