89842c060044d8bd4873f1232d8b8202a525e8c2
Gary D. Gregory <ggregory@apache.org>
2012-06-20 17:45:13 +0000

    Fix Checkstyle issues: - First sentence should end with a period. - Line does not match expected header line of ' * Licensed to the Apache Software Foundation (ASF) under one or more'.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1352230 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/commons/vfs2/libcheck/SftpCheck.java
+++ examples/src/main/java/org/apache/commons/vfs2/libcheck/SftpCheck.java
@@ -1,5 +1,5 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
+ * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
@@ -26,7 +26,7 @@ import com.jcraft.jsch.Session;
 import com.jcraft.jsch.UserInfo;
 
 /**
- * Basic check for sftp
+ * Basic check for SFTP.
  */
 public class SftpCheck
 {
