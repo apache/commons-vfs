2ab596857e6752c8a0cbb8f2f8c13b808ba6399f
Joerg Schaible <joehni@apache.org>
2013-02-19 15:09:41 +0000

    Support https_proxy environment variable to pass test.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1447755 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/commons/vfs2/provider/https/test/GetContentInfoFunctionalTest.java
+++ core/src/test/java/org/apache/commons/vfs2/provider/https/test/GetContentInfoFunctionalTest.java
@@ -1,10 +1,16 @@
 package org.apache.commons.vfs2.provider.https.test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.commons.vfs2.FileContent;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.FileSystemOptions;
 import org.apache.commons.vfs2.VFS;
+import org.apache.commons.vfs2.provider.http.HttpFileSystemConfigBuilder;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,12 +27,34 @@ public class GetContentInfoFunctionalTest
      *
      * @throws FileSystemException
      *             thrown when the getContentInfo API fails.
+     * @throws MalformedURLException 
+     *             thrown when the System environment contains an invalid URL for an HTTPS proxy.
      */
     @Test
-    public void testGoogle() throws FileSystemException
+    public void testGoogle() throws FileSystemException, MalformedURLException
     {
+    	String httpsProxyHost = null;
+    	int httpsProxyPort = -1;
+        final String httpsProxy = System.getenv("https_proxy");
+        if (httpsProxy != null) {
+        	final URL url = new URL(httpsProxy);
+        	httpsProxyHost = url.getHost();
+        	httpsProxyPort = url.getPort();
+        }
+        final FileSystemOptions opts;
+        if (httpsProxyHost != null) {
+            opts = new FileSystemOptions();
+            final HttpFileSystemConfigBuilder builder = HttpFileSystemConfigBuilder.getInstance();
+            builder.setProxyHost(opts, httpsProxyHost);
+            if (httpsProxyPort >= 0) {
+            	builder.setProxyPort(opts, httpsProxyPort);
+            }
+        } else {
+        	opts = null;
+        }
+        
         final FileSystemManager fsManager = VFS.getManager();
-        final FileObject fo = fsManager.resolveFile("https://www.google.com/images/logos/ps_logo2.png");
+        final FileObject fo = fsManager.resolveFile("https://www.google.com/images/logos/ps_logo2.png", opts);
         final FileContent content = fo.getContent();
         Assert.assertNotNull(content);
         // Used to NPE before fix:
