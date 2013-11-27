9efb7620a96ead2c71e300b5f83779b058648565
Gary D. Gregory <ggregory@apache.org>
2013-11-27 20:02:35 +0000

    Fix Javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1546176 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/commons/vfs2/provider/hdfs/HdfsFileProvider.java
+++ core/src/main/java/org/apache/commons/vfs2/provider/hdfs/HdfsFileProvider.java
@@ -58,8 +58,8 @@ public class HdfsFileProvider extends AbstractOriginatingFileProvider
     }
 
     /**
-     * @see org.apache.commons.vfs2.provider.AbstractOriginatingFileProvider#
-     *      doCreateFileSystem(org.apache.commons.vfs2.FileName, org.apache.commons.vfs2.FileSystemOptions)
+     * @see org.apache.commons.vfs2.provider.AbstractOriginatingFileProvider#doCreateFileSystem(org.apache.commons.vfs2.FileName,
+     *      org.apache.commons.vfs2.FileSystemOptions)
      */
     @Override
     protected FileSystem doCreateFileSystem(final FileName rootName, final FileSystemOptions fileSystemOptions)
