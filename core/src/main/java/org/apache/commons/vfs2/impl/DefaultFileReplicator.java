b75712b5032dc492930ebb2715adb9aa524a7f76
Gary D. Gregory <ggregory@apache.org>
2013-11-28 03:52:31 +0000

    Sort members in AB order.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1546276 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/commons/vfs2/impl/DefaultFileReplicator.java
+++ core/src/main/java/org/apache/commons/vfs2/impl/DefaultFileReplicator.java
@@ -39,18 +39,22 @@ public class DefaultFileReplicator
     extends AbstractVfsComponent
     implements FileReplicator, TemporaryFileStore
 {
+    private static final Log log = LogFactory.getLog(DefaultFileReplicator.class);
+    private static final int MASK = 0xffff;
+
     private static final char[] TMP_RESERVED_CHARS = new char[]
         {
             '?', '/', '\\', ' ', '&', '"', '\'', '*', '#', ';', ':', '<', '>', '|'
         };
-    private static final Log log = LogFactory.getLog(DefaultFileReplicator.class);
-
-    private static final int MASK = 0xffff;
 
     private final ArrayList<Object> copies = new ArrayList<Object>();
-    private File tempDir;
     private long filecount;
+    private File tempDir;
     private boolean tempDirMessageLogged;
+
+    public DefaultFileReplicator()
+    {
+    }
 
     /**
      * constructor to set the location of the temporary directory.
@@ -62,8 +66,116 @@ public class DefaultFileReplicator
         this.tempDir = tempDir;
     }
 
-    public DefaultFileReplicator()
+    protected void addFile(final Object file)
     {
+        synchronized (copies)
+        {
+            copies.add(file);
+        }
+    }
+
+    /**
+     * Allocates a new temporary file.
+     * @param baseName the base file name.
+     * @return The created File.
+     * @throws FileSystemException if an error occurs.
+     */
+    @Override
+    public File allocateFile(final String baseName) throws FileSystemException
+    {
+        // Create a unique-ish file name
+        final String basename = createFilename(baseName);
+        synchronized (this)
+        {
+            filecount++;
+        }
+
+        return createAndAddFile(tempDir, basename);
+    }
+
+    /**
+     * Closes the replicator, deleting all temporary files.
+     */
+    @Override
+    public void close()
+    {
+        // Delete the temporary files
+        synchronized (copies)
+        {
+            while (copies.size() > 0)
+            {
+                final File file = (File) removeFile();
+                deleteFile(file);
+            }
+        }
+
+        // Clean up the temp directory, if it is empty
+        if (tempDir != null && tempDir.exists() && tempDir.list().length == 0)
+        {
+            tempDir.delete();
+            tempDir = null;
+        }
+    }
+
+    protected File createAndAddFile(final File parent, final String basename) throws FileSystemException
+    {
+        final File file = createFile(tempDir, basename);
+
+        // Keep track to delete later
+        addFile(file);
+
+        return file;
+    }
+
+    /**
+     * create the temporary file
+     * @param parent The file to use as the parent of the file being created.
+     * @param name The name of the file to create.
+     * @return The File that was created.
+     * @throws FileSystemException if an error occurs creating the file.
+     */
+    protected File createFile(final File parent, final String name) throws FileSystemException
+    {
+        return new File(parent, UriParser.decode(name));
+    }
+
+    /**
+     * create the temporary file name
+     * @param baseName The base to prepend to the file name being created.
+     * @return the name of the File.
+     */
+    protected String createFilename(final String baseName)
+    {
+        // BUG29007
+        // return baseName + "_" + getFilecount() + ".tmp";
+
+        // imario@apache.org: BUG34976 get rid of maybe reserved and dangerous characters
+        // e.g. to allow replication of http://hostname.org/fileservlet?file=abc.txt
+        final String safeBasename = UriParser.encode(baseName, TMP_RESERVED_CHARS).replace('%', '_');
+        return "tmp_" + getFilecount() + "_" + safeBasename;
+    }
+
+    /**
+     * physically deletes the file from the filesystem
+     * @param file The File to delete.
+     */
+    protected void deleteFile(final File file)
+    {
+        try
+        {
+            final FileObject fileObject = getContext().toFileObject(file);
+            fileObject.deleteAll();
+        }
+        catch (final FileSystemException e)
+        {
+            final String message = Messages.getString("vfs.impl/delete-temp.warn", file.getName());
+            VfsLog.warn(getLogger(), log, message, e);
+        }
+    }
+
+    protected long getFilecount()
+    {
+        return filecount;
     }
 
     /**
@@ -92,48 +204,6 @@ public class DefaultFileReplicator
     }
 
     /**
-     * Closes the replicator, deleting all temporary files.
-     */
-    @Override
-    public void close()
-    {
-        // Delete the temporary files
-        synchronized (copies)
-        {
-            while (copies.size() > 0)
-            {
-                final File file = (File) removeFile();
-                deleteFile(file);
-            }
-        }
-
-        // Clean up the temp directory, if it is empty
-        if (tempDir != null && tempDir.exists() && tempDir.list().length == 0)
-        {
-            tempDir.delete();
-            tempDir = null;
-        }
-    }
-
-    /**
-     * physically deletes the file from the filesystem
-     * @param file The File to delete.
-     */
-    protected void deleteFile(final File file)
-    {
-        try
-        {
-            final FileObject fileObject = getContext().toFileObject(file);
-            fileObject.deleteAll();
-        }
-        catch (final FileSystemException e)
-        {
-            final String message = Messages.getString("vfs.impl/delete-temp.warn", file.getName());
-            VfsLog.warn(getLogger(), log, message, e);
-        }
-    }
-
-    /**
      * removes a file from the copies list. Will be used for cleanup. <br/>
      * Notice: The system awaits that the returning object can be cast to a java.io.File
      * @return the File that was removed.
@@ -156,76 +226,6 @@ public class DefaultFileReplicator
         {
             copies.remove(file);
         }
-    }
-
-    /**
-     * Allocates a new temporary file.
-     * @param baseName the base file name.
-     * @return The created File.
-     * @throws FileSystemException if an error occurs.
-     */
-    @Override
-    public File allocateFile(final String baseName) throws FileSystemException
-    {
-        // Create a unique-ish file name
-        final String basename = createFilename(baseName);
-        synchronized (this)
-        {
-            filecount++;
-        }
-
-        return createAndAddFile(tempDir, basename);
-    }
-
-    protected File createAndAddFile(final File parent, final String basename) throws FileSystemException
-    {
-        final File file = createFile(tempDir, basename);
-
-        // Keep track to delete later
-        addFile(file);
-
-        return file;
-    }
-
-    protected void addFile(final Object file)
-    {
-        synchronized (copies)
-        {
-            copies.add(file);
-        }
-    }
-
-    protected long getFilecount()
-    {
-        return filecount;
-    }
-
-    /**
-     * create the temporary file name
-     * @param baseName The base to prepend to the file name being created.
-     * @return the name of the File.
-     */
-    protected String createFilename(final String baseName)
-    {
-        // BUG29007
-        // return baseName + "_" + getFilecount() + ".tmp";
-
-        // imario@apache.org: BUG34976 get rid of maybe reserved and dangerous characters
-        // e.g. to allow replication of http://hostname.org/fileservlet?file=abc.txt
-        final String safeBasename = UriParser.encode(baseName, TMP_RESERVED_CHARS).replace('%', '_');
-        return "tmp_" + getFilecount() + "_" + safeBasename;
-    }
-
-    /**
-     * create the temporary file
-     * @param parent The file to use as the parent of the file being created.
-     * @param name The name of the file to create.
-     * @return The File that was created.
-     * @throws FileSystemException if an error occurs creating the file.
-     */
-    protected File createFile(final File parent, final String name) throws FileSystemException
-    {
-        return new File(parent, UriParser.decode(name));
     }
 
     /**
