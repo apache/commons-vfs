fd6e00d416046f88f238a586b8913db5b469db4a
Gary D. Gregory <ggregory@apache.org>
2017-09-07 05:32:12 +0000

    Javadoc nit.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1807562 13f79535-47bb-0310-9956-ffa450edef68

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/zip/ZipFileObject.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/zip/ZipFileObject.java
@@ -80,7 +80,7 @@ public class ZipFileObject extends AbstractFileObject<ZipFileSystem> {
     /**
      * Determines if this file can be written to.
      *
-     * @return {@code true} if this file is writeable, {@code false} if not.
+     * @return {@code true} if this file is writable, {@code false} if not.
      * @throws FileSystemException
      *             if an error occurs.
      */
