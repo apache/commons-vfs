3a90bc874b4b8035b6b214cd8d96ec1dce194eda
Gary D. Gregory <ggregory@apache.org>
2018-11-20 14:18:59 +0000

    Javadoc and format nits.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/vfs/trunk@1847008 13f79535-47bb-0310-9956-ffa450edef68

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/ftps/FtpsFileSystem.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/ftps/FtpsFileSystem.java
@@ -30,7 +30,7 @@ import org.apache.commons.vfs2.provider.ftp.FtpFileSystem;
 public class FtpsFileSystem extends FtpFileSystem {
     
     /**
-     * Create a new FtpsFileSystem.
+     * Creates a new FtpsFileSystem.
      *
      * @param rootName The root of the file system.
      * @param ftpClient The FtpClient.
