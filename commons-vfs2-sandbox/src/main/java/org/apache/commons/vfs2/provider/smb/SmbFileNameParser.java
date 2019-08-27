db5b587d79a23d60b476c4281ebdea42a868c47d
Michiel Hendriks <michiel.hendriks@mp-objects.com>
2019-08-27 09:52:14 +0200

    Undo accidental code reformat

--- commons-vfs2-sandbox/src/main/java/org/apache/commons/vfs2/provider/smb/SmbFileNameParser.java
+++ commons-vfs2-sandbox/src/main/java/org/apache/commons/vfs2/provider/smb/SmbFileNameParser.java
@@ -28,63 +28,62 @@ import org.apache.commons.vfs2.provider.VfsComponentContext;
  * Implementation for sftp. set default port to 139
  */
 public class SmbFileNameParser extends URLFileNameParser {
-	private static final SmbFileNameParser INSTANCE = new SmbFileNameParser();
+    private static final SmbFileNameParser INSTANCE = new SmbFileNameParser();
+    private static final int SMB_PORT = 139;
 
-	private static final int SMB_PORT = 139;
+    public SmbFileNameParser() {
+        super(SMB_PORT);
+    }
 
-	public SmbFileNameParser() {
-		super(SMB_PORT);
-	}
+    public static FileNameParser getInstance() {
+        return INSTANCE;
+    }
 
-	public static FileNameParser getInstance() {
-		return INSTANCE;
-	}
+    @Override
+    public FileName parseUri(final VfsComponentContext context, final FileName base, final String filename)
+            throws FileSystemException {
+        final StringBuilder name = new StringBuilder();
 
-	@Override
-	public FileName parseUri(final VfsComponentContext context, final FileName base, final String filename)
-			throws FileSystemException {
-		final StringBuilder name = new StringBuilder();
+        // Extract the scheme and authority parts
+        final Authority auth = extractToPath(context, filename, name);
 
-		// Extract the scheme and authority parts
-		final Authority auth = extractToPath(context, filename, name);
+        // extract domain
+        String username = auth.getUserName();
+        final String domain = extractDomain(username);
+        if (domain != null) {
+            username = username.substring(domain.length() + 1);
+        }
 
-		// extract domain
-		String username = auth.getUserName();
-		final String domain = extractDomain(username);
-		if (domain != null) {
-			username = username.substring(domain.length() + 1);
-		}
+        // Decode and adjust separators
+        UriParser.canonicalizePath(name, 0, name.length(), this);
+        UriParser.fixSeparators(name);
 
-		// Decode and adjust separators
-		UriParser.canonicalizePath(name, 0, name.length(), this);
-		UriParser.fixSeparators(name);
+        // Extract the share
+        final String share = UriParser.extractFirstElement(name);
+        if (share == null || share.length() == 0) {
+            throw new FileSystemException("vfs.provider.smb/missing-share-name.error", filename);
+        }
 
-		// Extract the share
-		final String share = UriParser.extractFirstElement(name);
-		if (share == null || share.length() == 0) {
-			throw new FileSystemException("vfs.provider.smb/missing-share-name.error", filename);
-		}
+        // Normalise the path. Do this after extracting the share name,
+        // to deal with things like smb://hostname/share/..
+        final FileType fileType = UriParser.normalisePath(name);
+        final String path = name.toString();
 
-		// Normalise the path. Do this after extracting the share name,
-		// to deal with things like smb://hostname/share/..
-		final FileType fileType = UriParser.normalisePath(name);
-		final String path = name.toString();
+        return new SmbFileName(auth.getScheme(), auth.getHostName(), auth.getPort(), username, auth.getPassword(),
+                domain, share, path, fileType);
+    }
 
-		return new SmbFileName(auth.getScheme(), auth.getHostName(), auth.getPort(), username, auth.getPassword(),
-				domain, share, path, fileType);
-	}
+    private String extractDomain(final String username) {
+        if (username == null) {
+            return null;
+        }
 
-	private String extractDomain(final String username) {
-		if (username == null) {
-			return null;
-		}
+        for (int i = 0; i < username.length(); i++) {
+            if (username.charAt(i) == '\\') {
+                return username.substring(0, i);
+            }
+        }
 
-		for (int i = 0; i < username.length(); i++) {
-			if (username.charAt(i) == '\\') {
-				return username.substring(0, i);
-			}
-		}
-
-		return null;
-	}
+        return null;
+    }
 }
