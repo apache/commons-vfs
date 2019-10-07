619f303dea9336f54ef7276ac7968c9d265c5785
Gary Gregory <gardgregory@gmail.com>
2019-10-07 12:18:25 -0400

    Fix compiler warning.

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/util/WeakRefFileListener.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/util/WeakRefFileListener.java
@@ -63,8 +63,9 @@ public class WeakRefFileListener implements FileListener {
     protected FileListener getListener() throws Exception {
         final FileListener listener = this.listener.get();
         if (listener == null) {
-            final FileObject fileObject = fs.resolveFile(name);
-            fileObject.getFileSystem().removeListener(fileObject, this);
+            try (final FileObject fileObject = fs.resolveFile(name)) {
+                fileObject.getFileSystem().removeListener(fileObject, this);
+            }
         }
         return listener;
     }
