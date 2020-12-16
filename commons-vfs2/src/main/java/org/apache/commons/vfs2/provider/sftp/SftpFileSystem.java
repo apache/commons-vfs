d4f0f3ff606bffe5f248310fb52e8c97db7e32ef
Gary Gregory <garydgregory@gmail.com>
2020-12-16 17:50:37 -0500

    Fix Spotbugs issue.

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/sftp/SftpFileSystem.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/sftp/SftpFileSystem.java
@@ -18,6 +18,7 @@ package org.apache.commons.vfs2.provider.sftp;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -312,7 +313,7 @@ public class SftpFileSystem extends AbstractFileSystem {
         try {
             channel.setCommand(command);
             channel.setInputStream(null);
-            try (final InputStreamReader stream = new InputStreamReader(channel.getInputStream())) {
+            try (final InputStreamReader stream = new InputStreamReader(channel.getInputStream(), StandardCharsets.UTF_8)) {
                 channel.setErrStream(System.err, true);
                 channel.connect(connectTimeoutMillis);
 
