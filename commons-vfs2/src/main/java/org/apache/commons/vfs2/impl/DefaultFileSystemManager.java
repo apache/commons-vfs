9dc97a0b56800e574c343723bd62f7048dcb6b5c
Gary Gregory <garydgregory@gmail.com>
2020-12-31 11:28:16 -0500

    Use String#isEmpty().

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/impl/DefaultFileSystemManager.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/impl/DefaultFileSystemManager.java
@@ -787,7 +787,7 @@ public class DefaultFileSystemManager implements FileSystemManager {
         String scheme = UriParser.extractScheme(getSchemes(), buffer.toString());
 
         // Determine whether to prepend the base path
-        if (name.length() == 0 || (scheme == null && buffer.charAt(0) != FileName.SEPARATOR_CHAR)) {
+        if (name.isEmpty() || (scheme == null && buffer.charAt(0) != FileName.SEPARATOR_CHAR)) {
             // Supplied path is not absolute
             if (!VFS.isUriStyle()) {
                 // when using URIs the parent already do have the trailing "/"
