aa2ba0f7882be34e59be8a3ab02987600498eb13
Gary Gregory <garydgregory@gmail.com>
2021-02-04 21:37:43 -0500

    Move test packages to the same level as their main counterparts.
    This removes the ".test"

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/ram/CustomRamProviderTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/ram/CustomRamProviderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.vfs2.provider.ram.test;
+package org.apache.commons.vfs2.provider.ram;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
