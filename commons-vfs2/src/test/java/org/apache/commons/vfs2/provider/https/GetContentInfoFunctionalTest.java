a846bb29702f4000f5617e9c2450ed5b4347f113
Gary Gregory <garydgregory@gmail.com>
2021-02-04 21:36:32 -0500

    Move test packages to the same level as their main counterparts.
    This removes the ".test"

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/https/GetContentInfoFunctionalTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/https/GetContentInfoFunctionalTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.vfs2.provider.https.test;
+package org.apache.commons.vfs2.provider.https;
 
 import java.net.MalformedURLException;
 import java.net.URL;
