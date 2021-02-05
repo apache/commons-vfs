8ff11703acdb211322edf59c679ac577f9778374
Gary Gregory <garydgregory@gmail.com>
2021-02-04 21:36:03 -0500

    Move test packages to the same level as their main counterparts.
    This removes the ".test"

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/impl/DefaultFileSystemManagerTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/impl/DefaultFileSystemManagerTest.java
@@ -17,9 +17,12 @@
 
 package org.apache.commons.vfs2.impl;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 import org.apache.commons.vfs2.CacheStrategy;
+import org.apache.commons.vfs2.FileName;
+import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.VFS;
@@ -72,5 +75,43 @@ public class DefaultFileSystemManagerTest {
             Mockito.verify(provider).close();
             Assert.assertThrows(FileSystemException.class, () -> fileSystemManager.resolveFile("ram2:///"));
         }
+    }
+
+    /**
+     * Even if the file name is absolute, the base file must be given. This is an inconsistency in the API, but it is
+     * documented as such.
+     *
+     * @see "VFS-519"
+     */
+    @Test(expected = NullPointerException.class)
+    public void testResolveFileAbsoluteThrows() throws FileSystemException {
+        final String absolute = new File("/").getAbsoluteFile().toURI().toString();
+        VFS.getManager().resolveFile((File) null, absolute);
+    }
+
+    /**
+     * If the base name is {@code null}, the file system manager should fail throwing a FileSystemException.
+     *
+     * @see VFS-189
+     */
+    @Test(expected = FileSystemException.class)
+    public void testResolveFileNameNull() throws FileSystemException {
+        VFS.getManager().resolveName((FileName) null, "../");
+    }
+
+    @Test
+    public void testResolveFileObjectNullAbsolute() throws FileSystemException {
+        final String absolute = new File("/").getAbsoluteFile().toURI().toString();
+        VFS.getManager().resolveFile((FileObject) null, absolute);
+    }
+
+    @Test(expected = FileSystemException.class)
+    public void testResolveFileObjectRelativeThrows() throws FileSystemException {
+        VFS.getManager().resolveFile((FileObject) null, "relativePath");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testResolveFileRelativeThrows() throws FileSystemException {
+        VFS.getManager().resolveFile((File) null, "relativePath");
     }
 }
