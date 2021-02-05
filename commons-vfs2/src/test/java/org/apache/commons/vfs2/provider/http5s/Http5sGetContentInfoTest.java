925b4961c9773d6b642443d0f214e9dfdfbdf6e8
Gary Gregory <garydgregory@gmail.com>
2021-02-04 21:30:57 -0500

    Move test packages to the same level as their main counterparts.
    This removes the ".test"

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/http5s/Http5sGetContentInfoTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/http5s/Http5sGetContentInfoTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.vfs2.provider.http5s.test;
+package org.apache.commons.vfs2.provider.http5s;
 
 import java.io.BufferedReader;
 import java.io.IOException;
