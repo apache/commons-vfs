1ee8f4197218b591ed291ac9abe87809b73c40bb
Gary Gregory <garydgregory@gmail.com>
2021-02-04 21:37:14 -0500

    Move test packages to the same level as their main counterparts.
    This removes the ".test"

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/local/TempFileTests.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/local/TempFileTests.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.vfs2.provider.local.test;
+package org.apache.commons.vfs2.provider.local;
 
 import java.io.File;
 import java.net.URI;
--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/local/UrlTests.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/local/UrlTests.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.vfs2.provider.local.test;
+package org.apache.commons.vfs2.provider.local;
 
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemManager;
