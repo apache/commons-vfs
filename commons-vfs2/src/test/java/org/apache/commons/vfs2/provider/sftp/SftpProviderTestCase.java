e67ad1b468da7d7fcad19ee2595b01c62945d130
Gary Gregory <garydgregory@gmail.com>
2021-02-04 21:53:17 -0500

    Move test packages to the same level as their main counterparts.
    This removes the ".test"

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/sftp/SftpProviderTestCase.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/sftp/SftpProviderTestCase.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.vfs2.provider.sftp.test;
+package org.apache.commons.vfs2.provider.sftp;
 
 import org.apache.commons.vfs2.test.PermissionsTests;
 
