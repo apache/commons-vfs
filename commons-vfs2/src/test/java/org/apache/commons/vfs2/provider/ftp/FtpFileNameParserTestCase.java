2911ed060faee5240fb7bc7dc0855e83aeb63f1d
Gary Gregory <garydgregory@gmail.com>
2021-11-25 13:49:45 -0500

    [VFS-793] GenericFileName.getURI() returns invalid URI.
    See also [VFS-810].

---
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/ftp/FtpFileNameParserTestCase.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.vfs2.provider.ftp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.vfs2.FileName;
+import org.apache.commons.vfs2.provider.GenericFileName;
+import org.junit.Test;
+
+/**
+ * Tests https://issues.apache.org/jira/browse/VFS-793.
+ *
+ * See also https://issues.apache.org/jira/browse/VFS-810.
+ */
+public class FtpFileNameParserTestCase {
+
+    @Test
+    public void testGenericFileName1() throws Exception {
+        final String uri = "ftp://blanks:blanks@localhost/path/file_b%20lanks";
+        final FileName n = FtpFileNameParser.getInstance().parseUri(null, null, uri);
+        assertTrue(n instanceof GenericFileName);
+        final String genericUri = n.getURI();
+        assertEquals(genericUri, uri.toString());
+    }
+
+    @Test
+    public void testGenericFileName2() throws Exception {
+        final String uri = "ftp://b%20lanks:b%20lanks@localhost/path/file";
+        final FileName n = FtpFileNameParser.getInstance().parseUri(null, null, uri);
+        assertTrue(n instanceof GenericFileName);
+        final String genericUri = n.getURI();
+        assertEquals(genericUri, uri.toString());
+    }
+}
