aac795a856421411c7ae529287e5a2b71bf38039
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-02-20 23:49:52 +0000

    JUnit v5 FileObjectEscapeCharacterInPathTest

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/FileObjectEscapeCharacterInPathTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/FileObjectEscapeCharacterInPathTest.java
@@ -16,8 +16,8 @@
  */
 package org.apache.commons.vfs2;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
