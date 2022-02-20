ac11a6d19842031a0bd50b7d184d305a35094e5c
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-02-20 23:57:07 +0000

    JUnit v5 RandomAccessModeTest

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/util/RandomAccessModeTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/util/RandomAccessModeTest.java
@@ -16,11 +16,11 @@
  */
 package org.apache.commons.vfs2.util;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.AccessMode;
 
