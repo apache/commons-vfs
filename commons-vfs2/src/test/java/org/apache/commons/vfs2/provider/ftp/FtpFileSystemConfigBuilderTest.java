95d103d56d50f7fa144df6cdd93a5f5813e62dfa
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-02-20 23:50:49 +0000

    JUnit v5 FtpFileSystemConfigBuilderTest

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/ftp/FtpFileSystemConfigBuilderTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/ftp/FtpFileSystemConfigBuilderTest.java
@@ -16,7 +16,7 @@
  */
 package org.apache.commons.vfs2.provider.ftp;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
 
