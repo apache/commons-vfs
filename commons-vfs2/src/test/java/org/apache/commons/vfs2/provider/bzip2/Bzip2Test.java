68bf175d09ec7a41b3ae027e332dae39a5bfe1ec
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-02-20 20:16:47 +0000

    Java Code Style remove empty space before package

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/bzip2/Bzip2Test.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/bzip2/Bzip2Test.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.commons.vfs2.provider.bzip2;
 
 import static org.apache.commons.vfs2.VfsTestUtils.getTestResource;
