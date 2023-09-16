76c92f431ed6dc80d988619cedbbaa2501f64224
Gary Gregory <garydgregory@gmail.com>
2023-09-16 08:54:57 -0400

    [VFS-844] UriString of webdav-filename is twice URLEncoded in moveTo operation
    - Sort members
    - Use try-with-resources

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/ProviderRenameTests.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/ProviderRenameTests.java
@@ -97,23 +97,6 @@ public class ProviderRenameTests extends AbstractProviderTestCase {
     }
 
     /**
-     * Tests create-delete-create-a-file sequence on the same file system.
-     */
-    @Test
-    public void testRenameFileWithSpaces() throws Exception {
-        final FileObject scratchFolder = createScratchFolder();
-
-        // Create direct child of the test folder
-        final FileObject file = scratchFolder.resolveFile("file space.txt");
-        assertFalse(file.exists());
-
-        final String content = createTestFile(file);
-
-        // Make sure we can move the new file to another file on the same file system
-        moveFile(scratchFolder, file, content);
-    }
-
-    /**
      * Moves a file from a child folder to a parent folder to test what happens when the original folder is now empty.
      *
      * See [VFS-298] FTP: Exception is thrown when renaming a file.
@@ -141,20 +124,37 @@ public class ProviderRenameTests extends AbstractProviderTestCase {
      */
     @Test
     public void testRenameFileIntoEmptyFolder() throws Exception {
+        try (FileObject scratchFolder = createScratchFolder();
+                // Create direct child of the test folder
+                final FileObject file = scratchFolder.resolveFile("file1.txt")) {
+            assertFalse(file.exists());
+
+            final String content = createTestFile(file);
+
+            final FileObject destFolder = scratchFolder.resolveFile("empty-target-folder");
+            destFolder.createFolder();
+            assertTrue("new destination must be folder", destFolder.getType().hasChildren());
+            assertEquals("new destination must be empty", 0, destFolder.getChildren().length);
+
+            moveFile(destFolder, file, content);
+        }
+    }
+
+    /**
+     * Tests create-delete-create-a-file sequence on the same file system.
+     */
+    @Test
+    public void testRenameFileWithSpaces() throws Exception {
         final FileObject scratchFolder = createScratchFolder();
 
         // Create direct child of the test folder
-        final FileObject file = scratchFolder.resolveFile("file1.txt");
+        final FileObject file = scratchFolder.resolveFile("file space.txt");
         assertFalse(file.exists());
 
         final String content = createTestFile(file);
 
-        final FileObject destFolder = scratchFolder.resolveFile("empty-target-folder");
-        destFolder.createFolder();
-        assertTrue("new destination must be folder", destFolder.getType().hasChildren());
-        assertEquals("new destination must be empty", 0, destFolder.getChildren().length);
-
-        moveFile(destFolder, file, content);
+        // Make sure we can move the new file to another file on the same file system
+        moveFile(scratchFolder, file, content);
     }
 
 }
