424876ffac7c99ca0c454cffd809c2e692e672e3
Anthony Goubard <anthony.goubard@japplis.com>
2024-05-29 14:18:31 +0200

    Improved performance of UriParser.normalisePath

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/UriParser.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/UriParser.java
@@ -70,8 +70,8 @@ public final class UriParser {
             if (cursor + 2 >= end) {
                 return false;
             }
-            final String sub = path.substring(cursor, cursor + 3);
-            if (sub.equals("%2e") || sub.equals("%2E")) {
+            if (path.charAt(cursor) == '%' && path.charAt(cursor + 1) == '2' &&
+                    (path.charAt(cursor + 2) == 'E' || path.charAt(cursor + 2) == 'e')) {
                 cursor += 3;
                 return true;
             }
@@ -89,8 +89,7 @@ public final class UriParser {
                 cursor++;
                 return true;
             }
-            final String sub = path.substring(cursor + 1, cursor + 3);
-            if (sub.equals(URLENCODED_SLASH_UC) || sub.equals(URLENCODED_SLASH_LC)) {
+            if (isCursorAtUrlEncodedSlash()) {
                 return false;
             }
             cursor++;
@@ -115,8 +114,7 @@ public final class UriParser {
             if (cursor + 2 >= end) {
                 return false;
             }
-            final String sub = path.substring(cursor, cursor + 3);
-            if (sub.equals(URLENCODED_SLASH_LC) || sub.equals(URLENCODED_SLASH_UC)) {
+            if (isCursorAtUrlEncodedSlash()) {
                 cursor += 3;
                 return true;
             }
@@ -128,6 +126,11 @@ public final class UriParser {
             while (reading) {
                 reading = readNonSeparator();
             }
+        }
+
+        private boolean isCursorAtUrlEncodedSlash() {
+            return path.charAt(cursor) == '%' && path.charAt(cursor + 1) == '2' &&
+                    (path.charAt(cursor + 2) == 'F' || path.charAt(cursor + 2) == 'f');
         }
 
         private void removePreviousElement(final int to) throws FileSystemException {
@@ -669,11 +672,10 @@ public final class UriParser {
             if (maxlen > 1 && path.charAt(maxlen - 1) == SEPARATOR_CHAR) {
                 path.delete(maxlen - 1, maxlen);
             }
-            if (maxlen > 3) {
-                final String sub = path.substring(maxlen - 3);
-                if (sub.equals(URLENCODED_SLASH_UC) || sub.equals(URLENCODED_SLASH_LC)) {
-                    path.delete(maxlen - 3, maxlen);
-                }
+            if (maxlen > 3 &&
+                    path.charAt(maxlen - 3) == '%' && path.charAt(maxlen - 2) == '2' &&
+                    (path.charAt(maxlen - 1) == 'F' || path.charAt(maxlen - 1) == 'f')) {
+                path.delete(maxlen - 3, maxlen);
             }
         }
 
