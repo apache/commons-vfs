f9d115e389c297e0b190a1572854dc3e1c2c2269
Gary D. Gregory <garydgregory@gmail.com>
2025-05-25 15:43:47 -0400

    Update Apache License URL to HTTPS
    See https://www.apache.org/legal/src-headers.html

--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/http/HttpGetContentInfoFunctionalTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/http/HttpGetContentInfoFunctionalTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -66,7 +66,7 @@ public class HttpGetContentInfoFunctionalTest {
     @Test
     public void testGetContentInfo() throws FileSystemException, MalformedURLException {
         final FileSystemManager fsManager = VFS.getManager();
-        final String uri = "http://www.apache.org/licenses/LICENSE-2.0.txt";
+        final String uri = "https://www.apache.org/licenses/LICENSE-2.0.txt";
         try (FileObject fo = fsManager.resolveFile(uri, getOptionsWithProxy());
                 final FileContent content = fo.getContent()) {
             assertNotNull(content);
--- commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/https/HttpsGetContentInfoFunctionalTest.java
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/https/HttpsGetContentInfoFunctionalTest.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -70,7 +70,7 @@ public class HttpsGetContentInfoFunctionalTest {
     public void testGetContentInfo() throws FileSystemException, MalformedURLException {
         @SuppressWarnings("resource") // getManager() returns a global.
         final FileSystemManager fsManager = VFS.getManager();
-        final String uri = "http://www.apache.org/licenses/LICENSE-2.0.txt";
+        final String uri = "https://www.apache.org/licenses/LICENSE-2.0.txt";
         try (FileObject fo = fsManager.resolveFile(uri, getOptionsWithProxy());
              final FileContent content = fo.getContent()) {
             assertNotNull(content);
