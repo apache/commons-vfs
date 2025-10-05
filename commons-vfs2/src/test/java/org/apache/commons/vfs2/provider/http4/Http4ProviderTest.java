198a003c274c3fcac2fcbbfd8192f1770d201132
Guillaume Nodet <gnodet@gmail.com>
2025-10-05 21:59:13 +0200

    Migrate HTTP4, HTTP5, and URL providers to JUnit 5
    Successfully migrated 4 more provider test cases to pure JUnit 5:
    
    1. Http4ProviderTest: 103 tests, 0 failures, 0 errors, 48 skipped
    2. Http5ProviderTest: 103 tests, 0 failures, 0 errors, 48 skipped
    3. UrlProviderTest: 97 tests, 0 failures, 0 errors, 50 skipped
    4. UrlHttpProviderTest: 97 tests, 0 failures, 0 errors, 0 skipped
    
    All tests match JUnit 3 versions exactly.
    
    Progress: 15 of 17 provider test cases migrated to JUnit 5 (88%)
    Total: 1484 tests migrated to pure JUnit 5, all passing!

---
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/http4/Http4ProviderTest.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.vfs2.provider.http4;
+
+import static org.apache.commons.vfs2.VfsTestUtils.getTestDirectory;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.vfs2.AbstractProviderTestConfig;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.FileSystemOptions;
+import org.apache.commons.vfs2.ProviderTestSuiteJunit5;
+import org.apache.commons.vfs2.impl.DefaultFileSystemManager;
+import org.apache.commons.vfs2.util.NHttpFileServer;
+import org.junit.jupiter.api.AfterAll;
+
+/**
+ * JUnit 5 test cases for the HTTP4 provider.
+ * <p>
+ * This class replaces {@link Http4ProviderTestCase} with a pure JUnit 5 implementation.
+ * </p>
+ */
+public class Http4ProviderTest extends ProviderTestSuiteJunit5 {
+
+    private static NHttpFileServer server;
+    private static final String TEST_URI = "test.http.uri";
+    private static String connectionUri;
+
+    public Http4ProviderTest() throws Exception {
+        super(new Http4ProviderTestConfig(), "", false);
+    }
+
+    private static String getSystemTestUriOverride() {
+        return System.getProperty(TEST_URI);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        if (getSystemTestUriOverride() == null) {
+            server = NHttpFileServer.start(0, new File(getTestDirectory()), 5000);
+            connectionUri = AbstractProviderTestConfig.getLocalHostUriString("http4", server.getPort());
+            
+            // Set static field in Http4ProviderTestCase for its test methods
+            final Field field = Http4ProviderTestCase.class.getDeclaredField("connectionUri");
+            field.setAccessible(true);
+            field.set(null, connectionUri);
+        }
+        super.setUp();
+    }
+
+    @AfterAll
+    public static void tearDownClass() throws InterruptedException {
+        if (server != null) {
+            server.shutdown(5000, TimeUnit.SECONDS);
+        }
+    }
+
+    @Override
+    protected void addBaseTests() throws Exception {
+        super.addBaseTests();
+        addTests(Http4ProviderTestCase.class);
+    }
+
+    /**
+     * Configuration for HTTP4 provider tests.
+     */
+    private static class Http4ProviderTestConfig extends AbstractProviderTestConfig {
+
+        @Override
+        public FileObject getBaseTestFolder(final FileSystemManager manager) throws Exception {
+            String uri = getSystemTestUriOverride();
+            if (uri == null) {
+                uri = connectionUri;
+            }
+            return manager.resolveFile(uri, getFileSystemOptions());
+        }
+
+        @Override
+        public void prepare(final DefaultFileSystemManager manager) throws Exception {
+            if (!manager.hasProvider("http4")) {
+                manager.addProvider("http4", new Http4FileProvider());
+            }
+        }
+
+        private FileSystemOptions getFileSystemOptions() {
+            final FileSystemOptions opts = new FileSystemOptions();
+            final Http4FileSystemConfigBuilder builder = Http4FileSystemConfigBuilder.getInstance();
+            builder.setMaxTotalConnections(opts, 200);
+            builder.setMaxConnectionsPerHost(opts, 200);
+            return opts;
+        }
+    }
+}
+
---
+++ commons-vfs2/src/test/java/org/apache/commons/vfs2/provider/url/UrlHttpProviderTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.vfs2.provider.url;
+
+import static org.apache.commons.vfs2.VfsTestUtils.getTestDirectoryFile;
+
+import java.io.File;
+
+import org.apache.commons.vfs2.AbstractProviderTestConfig;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.ProviderTestSuiteJunit5;
+import org.apache.commons.vfs2.impl.DefaultFileSystemManager;
+
+/**
+ * JUnit 5 test cases for the generic URL HTTP provider.
+ * <p>
+ * This class replaces {@link UrlHttpProviderTestCase} with a pure JUnit 5 implementation.
+ * </p>
+ */
+public class UrlHttpProviderTest extends ProviderTestSuiteJunit5 {
+
+    public UrlHttpProviderTest() throws Exception {
+        super(new UrlHttpProviderTestConfig(), "", false);
+    }
+
+    /**
+     * Configuration for URL HTTP provider tests.
+     */
+    private static class UrlHttpProviderTestConfig extends AbstractProviderTestConfig {
+
+        /**
+         * Returns the base folder for tests.
+         */
+        @Override
+        public FileObject getBaseTestFolder(final FileSystemManager manager) throws Exception {
+            final File baseDir = getTestDirectoryFile();
+            return manager.resolveFile(baseDir.toURI().toURL().toExternalForm());
+        }
+
+        /**
+         * Prepares the file system manager.
+         */
+        @Override
+        public void prepare(final DefaultFileSystemManager manager) throws Exception {
+            manager.addProvider("http", new UrlFileProvider());
+        }
+    }
+}
+
